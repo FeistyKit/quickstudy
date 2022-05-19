f4faf67d38e8000906db1bea8483b690c2cbe764
FeistyKit <eeveebeevee33@gmail.com>
2022-05-19 12:56:42 -0400

    implement parsing one answer and add test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 use ncurses;
 use std::{fmt, iter};
 
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn parse_answer_only() {
+        let src = "[answer]".to_string();
+
+        let question = Parser::new(&src).next();
+
+        assert_eq!(Some(Ok(Question {dat: vec![(Option::<String>::None, Some("answer".to_string()))]})), question);
+    }
+}
+
 fn main() -> Result<(), String> {
     init()?;
 
     let q = Question::new(["q1", "q2", "q3"], ["a1", "a2", "a3"], true);
 
-    let answer = q.ask();
+    let mut scr = String::new();
+
+    let answer = q.ask(&mut scr);
 
     print("\n");
 
@@ -28,11 +44,6 @@ fn print<T: fmt::Display>(dat: T) {
     ncurses::addstr(&buf);
 }
 
-#[derive(Debug)]
-struct Question {
-    dat: Vec<(Option<String>, Option<String>)>,
-}
-
 const REGULAR_COLOR: i16 = 1;
 const HIGHLIGHTED_COLOR: i16 = 2;
 
@@ -46,7 +57,11 @@ fn init() -> Result<(), String> {
     }
     ncurses::start_color();
     ncurses::init_pair(REGULAR_COLOR, ncurses::COLOR_WHITE, ncurses::COLOR_BLACK);
-    ncurses::init_pair(HIGHLIGHTED_COLOR, ncurses::COLOR_BLACK, ncurses::COLOR_WHITE);
+    ncurses::init_pair(
+        HIGHLIGHTED_COLOR,
+        ncurses::COLOR_BLACK,
+        ncurses::COLOR_WHITE,
+    );
 
     Ok(())
 }
@@ -67,8 +82,83 @@ fn pad_str(i: String) -> String {
     t
 }
 
-impl Question {
+#[derive(Debug, PartialEq, Eq)]
+struct Question {
+    dat: Vec<(Option<String>, Option<String>)>,
+}
 
+#[derive(Debug)]
+struct Parser<'a> {
+    src: std::str::Lines<'a>,
+    current_line: iter::Peekable<std::str::Chars<'a>>,
+}
+
+type ParseResult = Result<Question, String>;
+
+impl<'a> Parser<'a> {
+    fn new(src: &'a str) -> Self {
+        Self {
+            src: src.lines(),
+            current_line: "".chars().peekable() // Never will be touched, and if it is, it'll throw an error.
+        }
+    }
+
+    fn parse_text(&mut self) -> Result<String, String> {
+        todo!()
+    }
+
+    fn parse_answer(&mut self) -> Result<String, String> {
+        assert_eq!(self.current_line.next(), Some('['));
+
+        let mut answer = String::new();
+
+        while let Some(ch) = self.current_line.next() {
+            match ch {
+                ']' => return Ok(answer),
+                _ => answer.push(ch),
+            }
+        }
+        Err(String::from("Unexpected end of answer!"))
+    }
+
+    fn parse_question(&mut self, line: &'a str) -> ParseResult {
+        let mut dat = Vec::new();
+
+        self.current_line = line.chars().peekable();
+
+        match self.current_line.peek() {
+            Some('[') => dat.push((None, Some(self.parse_answer()?))),
+            Some(_) => {
+                let text = self.parse_text()?;
+
+                if self.current_line.peek().is_some() {
+                    // Has some characters left to consume
+                    dat.push((Some(text), Some(self.parse_answer()?)));
+                } else {
+                    // finished
+                    dat.push((Some(text), None));
+                }
+            }
+            None => unreachable!(),
+        }
+
+        Ok(Question {
+            dat
+        })
+    }
+}
+
+impl<'a> iter::Iterator for Parser<'a> {
+    type Item = Result<Question, String>;
+
+    fn next(&mut self) -> Option<ParseResult> {
+        let line = self.src.next()?;
+
+        Some(self.parse_question(line))
+    }
+}
+
+impl Question {
     fn new<T: ToString>(
         questions: impl iter::IntoIterator<Item = T>,
         answers: impl iter::IntoIterator<Item = T>,
@@ -80,7 +170,6 @@ impl Question {
         let mut dat = Vec::new();
 
         loop {
-
             let next_q = if answer_first {
                 answer_first = false;
                 None
@@ -93,15 +182,11 @@ impl Question {
             if next_q.is_some() || next_a.is_some() {
                 dat.push((next_q, next_a));
             } else {
-                break
+                break;
             }
-
         }
 
-        Self {
-            dat
-        }
-
+        Self { dat }
     }
 
     fn ask(&self, screen: &mut String) -> bool {
@@ -112,13 +197,9 @@ impl Question {
         self.render_partially_answered(&answers, &current, &*screen);
 
         for (_, answer) in &self.dat {
-
             if let Some(a) = answer {
-
                 'word: loop {
-
                     let ch = get_char();
-
 
                     match ch {
                         '\n' => {
@@ -132,8 +213,7 @@ impl Question {
                             self.render_partially_answered(&answers, &current, &*screen);
 
                             break 'word;
-
-                        },
+                        }
                         _ => {
                             if ch as u32 == 127 {
                                 // TODO: Going back and editing previous answers
@@ -146,7 +226,7 @@ impl Question {
                             } else {
                                 current.push(ch);
                             }
-                        },
+                        }
                     }
 
                     self.render_partially_answered(&answers, &current, &*screen);
@@ -157,7 +237,6 @@ impl Question {
     }
 
     fn render_partially_answered(&self, answers: &[String], current: &str, scr: &str) {
-
         let mut to_render = String::new();
         let mut pos = 0;
 
@@ -177,7 +256,7 @@ impl Question {
                         ncurses::attron(attr);
                         ncurses::addch(' ' as u32);
                         ncurses::attroff(attr);
-                    },
+                    }
                     std::cmp::Ordering::Greater => to_render.push_str("___"),
                 }
                 pos += 1;
